71a14cd66a814d0e95c4494ae603bdace7f384f0
FunnyBananchik <ya.antraktida2012.com@yandex.com>
2023-11-10 19:03:41 +0300

    Add task2

--- lab4/task2.sh
+++ lab4/task2.sh
@@ -1,1 +1,24 @@
 #!/bin/bash
+echo 'Введите название предмета'
+read  SubjectName
+echo 'Введите номер группы'
+read Ngroup
+for i in {1..4}
+do
+unset students
+declare -A students
+echo Test-$i
+grep  "${Ngroup}" labfiles/$SubjectName/tests/TEST-$i | grep "2$" > grep.txt
+for n in $(cat grep.txt)
+do
+p=$(echo $n | awk -F ";" '{print $2}')
+students[$p]=$(grep -c "$p" grep.txt)
+done
+for key in "${!students[@]}"
+do
+echo $key >> vivod.txt
+done
+cat vivod.txt | sort
+echo '------------------------------------------------------'
+rm -f grep.txt vivod.txt
+done
