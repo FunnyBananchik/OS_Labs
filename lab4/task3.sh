7e6e7cbea21c0eacaa66ebd382a8623320fcdaf4
FunnyBananchik <ya.antraktida2012.com@yandex.com>
2023-11-10 20:37:54 +0300

    Add task3

--- lab4/task3.sh
+++ lab4/task3.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+echo 'Введите название предмета'
+read  SubjectName
+echo 'Введите фамилию студента'
+read FIO
+data=$(grep -h "$FIO" labfiles/$SubjectName/*-attendance  | awk -F " " '{print $2}')
+i=0
+echo 'Номера пропущенных занятий'
+while [ $i -lt ${#data} ]
+do
+if [ ${data:i:1} -eq 0 ]
+then 
+if [ $((i+2)) -ne ${#data} ]
+then
+echo -n "$((i+1)), "
+else
+echo -n "$((i+1))"
+fi
+fi
+((i++))
+done
