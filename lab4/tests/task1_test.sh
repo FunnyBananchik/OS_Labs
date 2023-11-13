c14cd3fe161d574662659612ff0215ff3a7f2fa7
S-man-S <zinchenkosemen11@gmail.com>
2023-11-13 14:22:04 +0300

    Work with tests

--- lab4/tests/task1_test.sh
+++
@@ -1,7 +0,0 @@
-cd ..
-echo ----------------------------------------------*task1_test
-echo ----------------------------------------------*test1
-diff <(bash task1.sh) <(echo -e "Максимальное кол-во оценок 4 = 3\nKrivosheevAV") -s
-echo ----------------------------------------------*test2
-diff <(bash task1.sh) <(echo -e "Максимальное кол-во оценок 5 = 3\nSanjiyevMJ\nDereberiaTM") -s
-cd tests
