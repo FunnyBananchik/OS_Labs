32acddb07575c0955eb0dc2ebce1a8136f1756e8
S-man-S <zinchenkosemen11@gmail.com>
2023-11-14 00:02:41 +0300

    Some fixes

--- lab4/task4_test.sh
+++ lab4/task4_test.sh
@@ -5,5 +5,7 @@ echo ----------------------------------------------*test2
 diff <(bash task4.sh A-09-20) <(echo -e "1.	MukhamejanovAI\n2.	KharitonovAnA\n3.	TikhonovAVad\n4.	GrebeniukKS\n5.	MikhailovPA\n6.	FurtayevIV\n7.	GumerovaEF\n8.	LabanovaMS\n9.	SukhanovVS\n10.	AslanovAF") -s
 echo ----------------------------------------------*test3
 diff <(bash task4.sh A-F-K) <(echo -e "1.	AwesomeBL\n2.	ZubenkoMP\n3.	NiceDK\n4.	AboBA") -s
-echo ----------------------------------------------*test2
+echo ----------------------------------------------*test4
 diff <(bash task4.sh A-06-08) <(echo -e "1.	MalakhovaFR\n2.	PogrebnovWM\n3.	ZabolotnyDY\n4.	LimonovJM\n5.	LidinaSM\n6.	ShipovUU\n7.	DubovNO\n8.	PodaKS") -s
+echo ----------------------------------------------*test5
+diff <(bash task4.sh) <(echo -e "1.	Surovtsev-ButovA\n2.	SemichayevskyFF\n3.	ChakolyadenisE\n4.	CherenchikovJH\n5.	MukhamejanovAI\n6.	NazhmetdinovMA\n7.	PodshivalovaBH\n8.	PodshivalovaBZ\n9.	PodshivalovaOR\n10.	AbramenkoSemV") -s
