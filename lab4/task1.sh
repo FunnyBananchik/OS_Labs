13fa1a4cc80ad7e8c3b66e07622f9737e51d4f7b
FunnyBananchik <ya.antraktida2012.com@yandex.com>
2023-11-13 15:02:22 +0300

    Adding Exception Handling to task1

--- lab4/task1.sh
+++ lab4/task1.sh
@@ -1,6 +1,10 @@
 ﻿#!/bin/bash
 
 grep -h "$2" labfiles/$1/tests/TEST* | grep "${3}$" > grep.txt
+if [ ! -s grep.txt ]
+then
+echo "Максимальное кол-во оценок $3 = 0"
+else
 declare -A students
 for n in $(cat grep.txt)
 do
@@ -13,6 +17,10 @@ do
 echo $value >> mas.txt
 done
 max=$(sort -r mas.txt | head -1)
+#if [ "$max" == "0" ]
+#then
+# echo 'Нет людей, получивших оценку $3 '
+#else
 echo "Максимальное кол-во оценок $3 = $max"
 for student in "${!students[@]}"
 do
@@ -23,4 +31,6 @@ echo $student >> res.txt
 fi
 done
 cat res.txt | sort
-rm -f grep.txt mas.txt res.txt
+rm res.txt
+fi
+rm -f grep.txt mas.txt
