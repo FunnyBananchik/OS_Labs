2cb92ed77436030b6fa1fcf3e5d89c50f7737e50
S-man-S <zinchenkosemen11@gmail.com>
2023-11-29 20:11:56 +0300

    Change lab5_run

--- lab5/lab5_run.sh
+++ lab5/lab5_run.sh
@@ -1,4 +1,4 @@
-nasm code.asm -f bin -o code.bin
-xxd code.bin
-qemu-system-x86_64 code.bin
-rm -f code.bin
+nasm lab5.asm -f bin -o lab5.bin
+#xxd lab5.bin
+qemu-system-x86_64 lab5.bin
+#rm -f lab5.bin
